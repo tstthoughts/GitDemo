5a610b2e4c8c3050e4337a3212374232f9c93d6f
satya <sathya.thallam@gmail.com>
2020-12-28 15:41:13 +0530

    latest com

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -10,7 +10,8 @@ import io.cucumber.junit.CucumberOptions;
 		glue = "stepDefinitions",
 		tags = "@Search",
 		monochrome = true,
-		plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json", "json:target/cukes.xml"}
+		plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json", "json:target/cukes.xml"},
+		publish=true
 		)
 public class TestRunner {
 
