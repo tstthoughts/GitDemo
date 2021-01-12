110dbfe2250c1f0be989a58f74478ed755c63315
satya <sathya.thallam@gmail.com>
2021-01-12 21:56:52 +0530

    missed files added

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,8 +1,16 @@
 package cucumberOptions;
+
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import com.vimalselvam.cucumber.listener.Reporter;
+
+import Cucumber.Automation.FileReaderManager;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -10,9 +18,18 @@ import io.cucumber.junit.CucumberOptions;
 		glue = "stepDefinitions",
 		tags = "@Search",
 		monochrome = true,
-		plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json", "json:target/cukes.xml"},
-		publish=true
-		)
+		dryRun = false,
+		plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumberReport.json", "json:target/cukesReport.xml" }, publish = true)
+//"com.vimalselvam.cucumber.ExtentCucumberFormatter:target/cucumber-reports/report.html"
+// for debugging :: F5 for moving into a function // F6 for next step in the execution  //F8 to resume to run
 public class TestRunner {
-
+	/*
+	 * @AfterClass public static void writeExtentReport() {
+	 * Reporter.loadXMLConfig(new
+	 * File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath())
+	 * ); //another way above step delcaration :: Reporter.loadXMLConfig(new
+	 * File("config/extent-config.xml"));
+	 * 
+	 * }
+	 */
 }
--- src/test/java/stepDefinitions/hooks.java
+++ src/test/java/stepDefinitions/hooks.java
@@ -1,39 +1,53 @@
 package stepDefinitions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+
 import Cucumber.Automation.Base;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
-public class hooks extends Base{
-
+public class hooks extends Base {
+	 OutputStream fos = null;
+	 static String projectPath = System.getProperty("user.dir");
+	 File file = new File(projectPath+"\\src\\test\\java\\Cucumber\\Automation\\global.properties");
 	@Before("@MobileTest")
-	public void  beforeValidation()
-	{
+	public void beforeValidation() {
 		System.out.println("***Mobile before Hook***");
 	}
-	
+
 	@After("@MobileTest")
-	public void  afterValidation()
-	{
+	public void afterValidation() {
 		System.out.println("***Mobile after Hook***");
 	}
-	
-	@After("@Search")
-	public void  afterSeleniumTest()
-	{
+
+	@After()  //"@Search"
+	public void afterSeleniumTest() throws IOException {
 		driver.close();
+		System.out.println("browser closed");
+		
+		//just for knowledge
+		/*
+		 * prop.setProperty("Result", "Passed"); fos = new FileOutputStream(file);
+		 * prop.store(fos, null);
+		 */
+		driver.quit();
 	}
 	
 
-	@Before("@WebTest")
-	public void  beforeWValidation()
-	{
-		System.out.println("***Web before Hook***");
+	@Before() //"@WebTest"
+	public void beforeWValidation() throws IOException {
+		driver = Base.getDriver();
+		//System.out.println("***Web before Hook***");
 	}
-	
+
 	@After("@WebTest")
-	public void  afterWValidation()
-	{
+	public void afterWValidation() {
 		System.out.println("***Web after Hook***");
 	}
 }
